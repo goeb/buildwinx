0d124e7e1b4263ea048c5cc8a2a96dcbb931c439
Frederic Hoerni <fhoerni@free.fr>
2017-08-20 15:38:54 +0200

    zlib: fix build with static flag

--- package/libzip.mk
+++ package/libzip.mk
@@ -5,6 +5,7 @@ SOURCE = libzip-$(VERSION).tar.xz
 
 # depends on zlib
 CONF_OPTS += --with-zlib=$(abspath $(DIR_INSTALL))
+CONF_OPTS += CFLAGS=-DZIP_STATIC
 CONF_OPTS += LIBS="-L$(abspath $(DIR_INSTALL))/lib -lz"
 
 
