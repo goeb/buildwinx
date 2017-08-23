37eea804225a67d532a4e32ebe1806f56d336308
Frederic Hoerni <fhoerni@free.fr>
2017-08-23 22:37:19 +0200

    poppler: fix flags

--- package/poppler.mk
+++ package/poppler.mk
@@ -6,7 +6,6 @@ SOURCE = poppler-$(VERSION).tar.xz
 CONF_OPTS += --enable-libopenjpeg=none
 CONF_OPTS += --disable-libtiff
 CONF_OPTS += --disable-libcurl
-CONF_OPTS += --disable-libjpeg
 CONF_OPTS += --disable-libpng
 CONF_OPTS += --disable-splash-output
 CONF_OPTS += --disable-cairo-output
@@ -19,6 +18,9 @@ CONF_OPTS += --without-x
 CONF_OPTS += --enable-dctdecoder=none
 CONF_OPTS += FREETYPE_CFLAGS=-I$(DIR_INSTALL)/include
 CONF_OPTS += FREETYPE_LIBS="-L$(DIR_INSTALL)/lib -lfreetype"
+CONF_OPTS += CFLAGS=-I$(DIR_INSTALL)/include
+CONF_OPTS += LIBS="-L$(DIR_INSTALL)/lib -liconv"
+CONF_OPTS += PKG_CONFIG_LIBDIR="$(DIR_INSTALL)/lib/pkgconfig"
 
 
 include tools/common.mk
