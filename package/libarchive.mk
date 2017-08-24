7ad7820f5315341adf172bd5a43f2cb20da7cfea
Frederic Hoerni <fhoerni@free.fr>
2017-08-24 23:45:15 +0200

    libarchive: fix pkconfig lib dir

--- package/libarchive.mk
+++ package/libarchive.mk
@@ -3,7 +3,7 @@ VERSION = 3.2.1
 SITE = http://www.libarchive.org/downloads
 SOURCE = libarchive-$(VERSION).tar.gz
 
-CONF_OPTS += PKG_CONFIG_LIBDIR=$(DIR_INSTALL)
+CONF_OPTS += PKG_CONFIG_LIBDIR=$(DIR_INSTALL)/lib/pkgconfig
 
 
 include tools/common.mk
