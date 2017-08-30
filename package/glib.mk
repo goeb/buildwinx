294d845ae44d8e4fd8ff9a8f770af717f6403601
Frederic Hoerni <fhoerni@free.fr>
2017-08-30 21:36:47 +0200

    glib: build without pcre

--- package/glib.mk
+++ package/glib.mk
@@ -7,5 +7,6 @@ SOURCE = glib-$(VERSION).tar.xz
 CONF_OPTS += PKG_CONFIG_LIBDIR=$(DIR_INSTALL)/lib/pkgconfig
 CONF_OPTS += CFLAGS=-I$(DIR_INSTALL)/include
 CONF_OPTS += LDFLAGS="-L$(DIR_INSTALL)/lib -lintl"
+CONF_OPTS += --without-pcre
 
 include tools/common.mk
